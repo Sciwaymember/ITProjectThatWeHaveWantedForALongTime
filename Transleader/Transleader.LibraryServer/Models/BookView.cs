659efb7a360eaf09bc5297f0dd6cc30f5222dda1
Sciwaymember <fomenkov.cmds@gmail.com>
2022-10-31 20:10:23 +0200

    Main: initialized the solution based on two joint projects of react and ASP. Added basic presentation layer models for book series and book instances. Created a test base stub class that stores a list of all books. Added an endpoint returning a response with an array of book series from the test database

---
+++ Transleader/Transleader.LibraryServer/Models/BookView.cs
@@ -0,0 +1,22 @@
+﻿namespace Transleader.LibraryServer.Models
+{
+    public class BookView
+    {
+        public string Title => Books.First().Title;
+
+        public string? Description => Books.First().Description;
+        
+        public string Author => Books.First().Author;
+
+        public string? Picture => Books.First().Picture;
+
+        public IList<BookInstanceView> Books { get; set;}
+
+        public int Count { get => Books.Count; }
+
+        public BookView(BookInstanceView book)
+        {
+            Books = new List<BookInstanceView>{book};
+        }
+    }
+}
